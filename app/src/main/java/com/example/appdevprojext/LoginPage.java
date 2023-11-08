5a95202ffc2f236d1b914a723ba9052e8926f69d
Hannah <helcano37@gmail.com>
2023-11-08 10:40:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/appdevprojext/LoginPage.java
@@ -0,0 +1,44 @@
+package com.example.appdevprojext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+    private Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        button1 = (Button) findViewById(R.id.loginBtn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.createaccBtn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCreateAccount();
+            }
+        });
+    }
+
+    public void openHomePage() {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
+    public void openCreateAccount() {
+        Intent intent2 = new Intent(this, CreateAccount.class);
+        startActivity(intent2);
+    }
+}
